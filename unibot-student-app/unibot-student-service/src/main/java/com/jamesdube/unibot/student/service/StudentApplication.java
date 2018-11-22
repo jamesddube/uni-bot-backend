0ae7a15e726d7d43b37bde493e605d2c70a48f14
James Dube <jamesdylandube@gmail.com>
2018-11-22 15:08:06 +0200

    fix dependency issues

---
+++ unibot-student-app/unibot-student-service/src/main/java/com/jamesdube/unibot/student/service/StudentApplication.java
@@ -0,0 +1,14 @@
+package com.jamesdube.unibot.student.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class StudentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentApplication.class,args);
+    }
+}
---
+++ unibot-student-app/unibot-student-service/src/main/test/com/jamesdube/unibot/student/service/StudentTest.java
@@ -0,0 +1,13 @@
+package com.jamesdube.unibot.student.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+@RunWith(SpringRunner.class)
+public class StudentTest {
+
+    @Test
+    public void contextLoads(){
+
+    }
+}
